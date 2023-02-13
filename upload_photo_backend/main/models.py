fb27cb12572a91dd31634ecb217941eda8b43cf6
Bartek7655 <bartekk194@gmail.com>
2023-02-13 19:01:25 +0100

    models done

--- upload_photo_backend/main/models.py
+++ upload_photo_backend/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from PIL import Image
 
-# Create your models here.
+
+class TypeAccount(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='user')
+
+
+class SizeImage(models.Model):
+    binary_link = models.BooleanField(default=False)
+    length = models.IntegerField()
+    width = models.IntegerField()
+    type_account = models.ManyToManyField(TypeAccount, related_name='sizes')
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+
+        img.thumbnail(100, 100)
+        img.save(self.image.path.replace(".", "_100x100."), "JPEG")
